e54b575ecf76d14ba371bb79608882d2196b5ea4
Jonathan Terrell <terrell.jm@gmail.com>
2025-12-08 19:57:13 +0100

    v0.0.21

--- eslint.config.ts
+++ eslint.config.ts
@@ -6,12 +6,4 @@
 import datapos from '@datapos/eslint-config-datapos';
 
 // Exposures - Configuration.
-export default [
-    ...datapos,
-    {
-        rules: {
-            'sonarjs/todo-tag': 'warn',
-            'unicorn/prefer-switch': 'warn'
-        }
-    }
-];
+export default [...datapos];
